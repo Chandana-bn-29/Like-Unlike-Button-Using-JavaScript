d85df030251cde09170ff60bc6fe2d375bc3e1ca
Chandana-bn-29 <chandanabn2904@gmail.com>
2025-11-18 00:16:35 +0530

    Add files via upload
    Add Like–Unlike button project with dynamic counter using JavaScript

---
+++ Creating LikeUnlike Button using JS/js/script.js
@@ -0,0 +1,24 @@
+const likeButton = document.querySelector('.btn-like');
+const unlikeButton = document.querySelector('.btn-unlike');
+
+const likeCount =document.querySelector('.like-count');
+// console.log(likeCount);
+
+var likes=0;
+
+
+likeButton.addEventListener('click',()=>{
+    console.log('like button clicked');
+    likes++;
+    likeCount.innerHTML=`${likes} Likes`;
+});
+
+unlikeButton.addEventListener('click',()=>{
+    console.log('unlike button clicked');
+    
+    if(likes>1){
+        likes--;
+    }
+    likeCount.innerHTML=`${likes} Likes`;
+
+});
